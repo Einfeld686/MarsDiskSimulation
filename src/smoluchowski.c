7f05bcdf1067fb14a3d150c23255a45004786476
Einfeld686 <130147986+Einfeld686@users.noreply.github.com>
2025-07-27 15:24:33 +0900

    Update README to reflect test availability

--- src/smoluchowski.c
+++ src/smoluchowski.c
@@ -10,11 +10,13 @@ void smol_init(SmolData* d){
     }
 }
 
+static double v_rel(const SmolData* d,int i,int j) __attribute__((unused));
 static double v_rel(const SmolData* d,int i,int j){
     return sqrt(d->v[i]*d->v[i] + d->v[j]*d->v[j]);
 }
 
 void smol_step(SmolData* d, double dt, double t, const char* prefix){
+    (void)dt;
     char fname[64];
     snprintf(fname,sizeof(fname),"output/%s_%06d.dat",prefix,(int)t);
     FILE* fp=fopen(fname,"w");
