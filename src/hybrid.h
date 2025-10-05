846da7b2bfb6f569d013cbe788762db8430e110b
北原 大地 <130147986+Einfeld686@users.noreply.github.com>
2025-10-05 23:22:17 +0900

    Delete src directory

--- src/hybrid.h
+++
@@ -1,9 +0,0 @@
-#ifndef HYBRID_H
-#define HYBRID_H
-
-#include "smoluchowski.h"
-#include "../rebound/src/rebound.h"
-
-void hybrid_step(struct reb_simulation* r, SmolData* d);
-
-#endif /* HYBRID_H */
